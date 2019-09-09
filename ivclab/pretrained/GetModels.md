8a8cf7ac7d9eb0b642a9f90136e420c7f39b1ceb
531851 <fabien.grisard@umons.ac.be>
2019-09-09 15:31:30 +0200

    remove unlicensed files

--- ivclab/pretrained/GetModels.md
+++
@@ -1,1 +0,0 @@
-Please download the pretrained models from authors [release page](https://github.com/ivclab/BigGAN-Generator-Pretrained-Pytorch/releases/tag/v0.0.0).
--- ivclab/run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set size=512
-
-mkdir D:\bigGANgenerated\%size%
-call activate biggan
-for /l %%i in (0, 1, 999) do python demo.py -w ./pretrained/biggan%size%-release.pt -s %size% -t 0.96 -c %%i -o D:\bigGANgenerated\%size%\class_%%i.png
-call deactivate
